5692856fbb649e4594b486c856e6ab7e672bf820
firuzinho <firuzinhokodirov@gmail.com>
2021-02-26 01:17:03 +0700

    revert

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,7 @@
 // // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
-
-// // Import vendor jQuery plugin example (not module)
-// require('~/app/libs/mmenu/dist/mmenu.js')
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
 
 document.addEventListener('DOMContentLoaded', () => {
 
